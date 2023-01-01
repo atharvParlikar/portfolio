32679d4e5a93fed8c96911ade32c52b714afbbc2
atharvParlikar <atharvparlikar@gmail.com>
2023-01-01 18:54:09 +0530

    Added macos close buttons

--- test/test.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-with open('test', 'r') as f:
-    list = f.read().split('\n')
-    for j, i in enumerate(list): 
-        subprocess.run(f'curl {i} > {i}.svg')
-        print(f'download {j}')
