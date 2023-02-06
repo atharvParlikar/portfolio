e8cb39e094fc7f499328c5aee5e26d496b2f34c6
Atharv Parlikar <atharvparlikar@gmail.com>
2023-02-06 20:33:13 +0530

    added mobile screen

---
+++ frontend/src/pages/Mobile.jsx
@@ -0,0 +1,12 @@
+const Mobile = () => {
+  return (
+      <div className="bg-gray-600 w-full h-screen font-UbuntuMono text-white text-center">
+        <div className="py-40">
+          <p>Sorry, my website isn't mobile responseive yet because im too lazy to make it so.</p>
+          <p>Visit it from a Desktop or laptop</p>
+        </div>
+      </div>
+  )
+}
+
+export default Mobile;
